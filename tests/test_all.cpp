78f2a1ad7c15004231c7c53f62548cc0be735825
Benjamin Schulz <beschulz@betabugs.de>
2015-03-15 17:48:56 +0100

    initial

---
+++ tests/test_all.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Benjamin Schulz on 14/03/15.
+//
+
+#ifndef BOOST_TEST_MODULE
+#	define BOOST_TEST_MODULE all_tests
+#	define BOOST_TEST_DYN_LINK
+#	include <boost/test/unit_test.hpp>
+#endif /* BOOST_TEST_MODULE */
+
+//#include "test_basics.cpp"
+//#include "test_limits.cpp"
