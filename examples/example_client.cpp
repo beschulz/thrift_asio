1f3fdc1b0d2ccbf95afcbf6a10b2a4c500dd7376
Benjamin Schulz <beschulz@betabugs.de>
2015-03-15 21:47:59 +0100

    implemented example handlers and main loops

--- examples/example_client.cpp
+++ examples/example_client.cpp
@@ -2,10 +2,88 @@
 // Created by Benjamin Schulz on 15/03/15.
 //
 
+#include <iostream>
+
+#include <betabugs/networking/thrift_asio_client.hpp>
+#include <chat_client.h>
+#include <chat_server.h>
+
+class chat_client_handler : public betabugs::networking::thrift_asio_client<
+	example::chat::chat_serverClient,
+	example::chat::chat_clientProcessor,
+	example::chat::chat_clientIf
+>
+{
+  public:
+	using betabugs::networking::thrift_asio_client<
+		example::chat::chat_serverClient,
+		example::chat::chat_clientProcessor,
+		example::chat::chat_clientIf
+	>::thrift_asio_client;
+
+
+	virtual void on_set_user_name_failed(const std::string& why) override
+	{
+		std::cerr << "setting user name failed: " << why << std::endl;
+	}
+
+	virtual void on_message(const std::string& from_user, const std::string& message) override
+	{
+		std::cout << from_user << ": " << message << std::endl;
+	}
+
+	virtual void on_send_message_failed(const std::string& why) override
+	{
+		std::cerr << "sending message failed: " << why << std::endl;
+	}
+
+	// called by the transport
+	virtual void on_error(const boost::system::error_code& ec) override
+	{
+		std::clog << "!! error: " << ec.message() << std::endl;
+		this->reconnect_in(1.0f);
+	}
+
+	virtual void on_connected() override
+	{
+		std::clog << "!! connected" << std::endl;
+		client_.set_user_name("I am the Source of foo!");
+	}
+
+	virtual void on_disconnected() override
+	{
+		std::clog << "!! disconnected" << std::endl;
+	}
+};
+
 int main(int argc, char* argv[])
 {
 	(void) argc;
 	(void) argv;
+
+	std::string host_name = "127.0.0.1";
+	std::string service_name = "1528";
+
+	{
+		boost::asio::io_service io_service;
+		boost::asio::io_service::work work(io_service);
+
+		chat_client_handler handler(io_service, host_name, service_name);
+
+		while (true)
+		{
+			boost::system::error_code ec;
+			while(io_service.poll_one(ec))
+			{
+				if (ec) handler.on_error(ec);
+			}
+
+			handler.update();
+
+			sleep(1);
+			std::clog << "loop" << std::endl;
+		}
+	}
 
 	return 0;
 }
--- examples/example_server.cpp
+++ examples/example_server.cpp
@@ -2,10 +2,91 @@
 // Created by Benjamin Schulz on 15/03/15.
 //
 
+#include <betabugs/networking/thrift_asio_server.hpp>
+#include <chat_server.h>
+#include <chat_client.h>
+
+class chat_server_handler
+	: public example::chat::chat_serverIf
+	, public betabugs::networking::thrift_asio_transport::event_handlers
+{
+
+  public:
+	virtual void set_user_name(const std::string& name) override
+	{
+
+	}
+
+	virtual void send_message(const std::string& to_user, const std::string& message) override
+	{
+
+	}
+
+	virtual void broadcast_message(const std::string& message) override
+	{
+
+	}
+
+	// functions called by thrift_asio_transport
+	virtual void on_error(const boost::system::error_code& ec)
+	{
+		std::clog << "thrift_asio_transport::on_error" << ec.message() << std::endl;
+	}
+
+	virtual void on_connected()
+	{
+		std::clog << "thrift_asio_transport::on_connected" << std::endl;
+	}
+
+	virtual void on_disconnected()
+	{
+		std::clog << "thrift_asio_transport::on_disconnected" << std::endl;
+	}
+
+	// functions called by thrift_asio_server
+	void on_client_connected(std::shared_ptr<betabugs::networking::thrift_asio_transport> transport)
+	{
+
+	}
+
+	void before_process(boost::shared_ptr<apache::thrift::protocol::TBinaryProtocol>)
+	{
+
+	}
+
+	void after_process()
+	{
+
+	}
+
+  private:
+	//example::chat::chat_clientClient
+};
+
 int main(int argc, char* argv[])
 {
 	(void) argc;
 	(void) argv;
+
+	auto handler = boost::make_shared<chat_server_handler>();
+	auto processor = example::chat::chat_serverProcessor
+		(
+			handler
+		);
+
+	betabugs::networking::thrift_asio_server<chat_server_handler> server;
+
+	boost::asio::io_service io_service;
+	boost::asio::io_service::work work(io_service);
+
+	server.serve(io_service, processor, handler, 1528);
+
+	while (true)
+	{
+		while (io_service.poll_one());
+		sleep(1);
+		std::clog << "loop" << std::endl;
+	}
 
 	return 0;
 }
