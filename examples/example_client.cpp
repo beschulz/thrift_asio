dd66abf4f707376fe49f3157a91f9e021a140b7d
Benjamin Schulz <beschulz@betabugs.de>
2015-03-16 00:52:35 +0100

    tweaked samples

--- examples/example_client.cpp
+++ examples/example_client.cpp
@@ -7,6 +7,8 @@
 #include <betabugs/networking/thrift_asio_client.hpp>
 #include <chat_client.h>
 #include <chat_server.h>
+#include <boost/asio.hpp>
+
 
 class chat_client_handler : public betabugs::networking::thrift_asio_client<
 	example::chat::chat_serverClient,
@@ -15,16 +17,32 @@ class chat_client_handler : public betabugs::networking::thrift_asio_client<
 >
 {
   public:
-	using betabugs::networking::thrift_asio_client<
+	/*using betabugs::networking::thrift_asio_client<
 		example::chat::chat_serverClient,
 		example::chat::chat_clientProcessor,
 		example::chat::chat_clientIf
-	>::thrift_asio_client;
+	>::thrift_asio_client;*/
+	chat_client_handler(
+		boost::asio::io_service& io_service,
+		const std::string& host_name,
+		const std::string& service_name
+	)
+		: thrift_asio_client(io_service, host_name, service_name)
+		, stdin_(io_service, ::dup(STDIN_FILENO))
+		, input_buffer_(1024)
+	{
+	}
 
 
 	virtual void on_set_user_name_failed(const std::string& why) override
 	{
 		std::cerr << "setting user name failed: " << why << std::endl;
+		read_username();
+	}
+
+	virtual void on_set_user_name_succeeded() override
+	{
+		read_message();
 	}
 
 	virtual void on_message(const std::string& from_user, const std::string& message) override
@@ -47,12 +65,49 @@ class chat_client_handler : public betabugs::networking::thrift_asio_client<
 	virtual void on_connected() override
 	{
 		std::clog << "!! connected" << std::endl;
-		client_.set_user_name("I am the Source of foo!");
+		read_username();
 	}
 
 	virtual void on_disconnected() override
 	{
 		std::clog << "!! disconnected" << std::endl;
+	}
+
+  private:
+	boost::asio::posix::stream_descriptor stdin_;
+	boost::asio::streambuf input_buffer_;
+
+	// read the username from stdin and send it to the server
+	void read_username()
+	{
+		std::cout << "enter user name: " << std::endl;
+		// Read a line of input entered by the user.
+		boost::asio::async_read_until(stdin_, input_buffer_, '\n',
+			[this](const boost::system::error_code& ec, std::size_t length)
+			{
+				std::istream is(&input_buffer_);
+				std::string line;
+				is >> line;
+				std::cout << line << std::endl;
+				client_.set_user_name(line);
+			}
+		);
+	}
+
+	void read_message()
+	{
+		std::cout << "broadcast message: " << std::endl;
+		// Read a line of input entered by the user.
+		boost::asio::async_read_until(stdin_, input_buffer_, '\n',
+			[this](const boost::system::error_code& ec, std::size_t length)
+			{
+				std::istream is(&input_buffer_);
+				std::string line;
+				is >> line;
+				std::cout << line << std::endl;
+				client_.broadcast_message(line);
+			}
+		);
 	}
 };
 
@@ -73,15 +128,15 @@ int main(int argc, char* argv[])
 		while (true)
 		{
 			boost::system::error_code ec;
-			while(io_service.poll_one(ec))
+			while (io_service.poll_one(ec))
 			{
 				if (ec) handler.on_error(ec);
 			}
 
 			handler.update();
 
-			sleep(1);
-			std::clog << "loop" << std::endl;
+			//sleep(1);
+			//std::clog << "loop" << std::endl;
 		}
 	}
 
--- examples/example_server.cpp
+++ examples/example_server.cpp
@@ -8,23 +8,48 @@
 
 class chat_server_handler
 	: public example::chat::chat_serverIf
-	, public betabugs::networking::thrift_asio_transport::event_handlers
+	  , public betabugs::networking::thrift_asio_transport::event_handlers
 {
 
   public:
 	virtual void set_user_name(const std::string& name) override
 	{
+		for (auto& p : sessions_)
+		{
+			if (p.second->user_name == name)
+			{
+				p.second->client.on_set_user_name_failed("username already taken");
+				return;
+			}
+		}
 
+		assert(current_session_);
+		current_session_->user_name = name;
+		current_session_->client.on_set_user_name_succeeded();
 	}
 
 	virtual void send_message(const std::string& to_user, const std::string& message) override
 	{
-
+		assert(current_session_);
+		for (auto& s : sessions_)
+		{
+			if( s.second->user_name == to_user )
+			{
+				s.second->client.on_message(current_session_->user_name, message);
+				return;
+			}
+		}
+		current_session_->client.on_send_message_failed("no such user " + to_user);
 	}
 
 	virtual void broadcast_message(const std::string& message) override
 	{
-
+		assert(current_session_);
+		for (auto& s : sessions_)
+		{
+			if (s.second != current_session_)
+				s.second->client.on_message(current_session_->user_name, message);
+		}
 	}
 
 	// functions called by thrift_asio_transport
@@ -44,23 +69,48 @@ class chat_server_handler
 	}
 
 	// functions called by thrift_asio_server
-	void on_client_connected(std::shared_ptr<betabugs::networking::thrift_asio_transport> transport)
+	void on_client_connected(boost::shared_ptr<apache::thrift::protocol::TProtocol> output_protocol)
 	{
-
+		sessions_.insert(
+			std::make_pair(
+				output_protocol,
+				std::make_shared<Session>(output_protocol, "no-name")
+			)
+		);
 	}
 
-	void before_process(boost::shared_ptr<apache::thrift::protocol::TBinaryProtocol>)
+	void before_process(boost::shared_ptr<apache::thrift::protocol::TBinaryProtocol> output_protocol)
 	{
-
+		Sessions::iterator pos = sessions_.find(output_protocol);
+		assert(pos != sessions_.end());
+		current_session_ = pos->second;
 	}
 
 	void after_process()
 	{
-
+		current_session_.reset();
 	}
 
   private:
-	//example::chat::chat_clientClient
+	typedef boost::shared_ptr<apache::thrift::protocol::TProtocol> protocol_ptr;
+
+	struct Session
+	{
+		Session(const example::chat::chat_clientClient& client, const std::string& user_name)
+			: client(client), user_name(user_name)
+		{
+		}
+
+		example::chat::chat_clientClient client;
+		std::string user_name;
+	};
+
+	typedef std::shared_ptr<Session> session_ptr;
+
+	typedef std::map<protocol_ptr, session_ptr> Sessions;
+	Sessions sessions_;
+
+	session_ptr current_session_;
 };
 
 int main(int argc, char* argv[])
