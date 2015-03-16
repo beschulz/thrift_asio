0007474603ef876f3191cfd66d13a387a510a5eb
Benjamin Schulz <beschulz@betabugs.de>
2015-03-16 22:19:25 +0100

    simplified example_server by employing the thrift_asio_connection_management_mixin

--- examples/example_server.cpp
+++ examples/example_server.cpp
@@ -3,19 +3,37 @@
 //
 
 #include <betabugs/networking/thrift_asio_server.hpp>
+#include <betabugs/networking/thrift_asio_connection_management_mixin.hpp>
 #include <chat_server.h>
 #include <chat_client.h>
 #include <thread> // for sleep
 
+/**
+* a chat-session holds the user name and a client
+* */
+struct session
+{
+	session(const boost::shared_ptr<apache::thrift::protocol::TProtocol>& output_protocol)
+		: client(output_protocol)
+	{
+
+	}
+
+	std::string user_name;
+	example::chat::chat_clientClient client;
+};
+
+
 class chat_server_handler
 	: public example::chat::chat_serverIf
 	  , public betabugs::networking::thrift_asio_transport::event_handlers
+	  , public betabugs::networking::thrift_asio_connection_management_mixin<session>
 {
 
   public:
 	virtual void set_user_name(const std::string& name) override
 	{
-		for (auto& p : sessions_)
+		for (auto& p : clients_)
 		{
 			if (p.second->user_name == name)
 			{
@@ -24,89 +42,20 @@ class chat_server_handler
 			}
 		}
 
-		assert(current_session_);
-		current_session_->user_name = name;
-		current_session_->client.on_set_user_name_succeeded();
+		assert(current_client_);
+		current_client_->user_name = name;
+		current_client_->client.on_set_user_name_succeeded();
 	}
 
 	virtual void broadcast_message(const std::string& message) override
 	{
-		assert(current_session_);
-		for (auto& s : sessions_)
+		assert(current_client_);
+		for (auto& s : clients_)
 		{
-			if (s.second != current_session_)
-				s.second->client.on_message(current_session_->user_name, message);
+			if (s.second != current_client_)
+				s.second->client.on_message(current_client_->user_name, message);
 		}
 	}
-
-	// functions called by thrift_asio_transport
-	virtual void on_error(const boost::system::error_code& ec)
-	{
-		std::clog << "thrift_asio_transport::on_error" << ec.message() << std::endl;
-	}
-
-	virtual void on_connected()
-	{
-		std::clog << "thrift_asio_transport::on_connected" << std::endl;
-	}
-
-	virtual void on_disconnected()
-	{
-		std::clog << "thrift_asio_transport::on_disconnected" << std::endl;
-	}
-
-	// functions called by thrift_asio_server
-	void on_client_connected(boost::shared_ptr<apache::thrift::protocol::TProtocol> output_protocol)
-	{
-		sessions_.insert(
-			std::make_pair(
-				output_protocol,
-				std::make_shared<Session>(output_protocol, "no-name")
-			)
-		);
-	}
-
-	void on_client_disconnected(const boost::shared_ptr<apache::thrift::protocol::TProtocol>& output_protocol, const boost::system::error_code& ec)
-	{
-		assert( sessions_.find(output_protocol) != sessions_.end() );
-
-		std::clog << "client disconnected. reason: " << ec.message() << std::endl;
-
-		sessions_.erase(output_protocol);
-	}
-
-	void before_process(boost::shared_ptr<apache::thrift::protocol::TBinaryProtocol> output_protocol)
-	{
-		Sessions::iterator pos = sessions_.find(output_protocol);
-		assert(pos != sessions_.end());
-		current_session_ = pos->second;
-	}
-
-	void after_process()
-	{
-		current_session_.reset();
-	}
-
-  private:
-	typedef boost::shared_ptr<apache::thrift::protocol::TProtocol> protocol_ptr;
-
-	struct Session
-	{
-		Session(const example::chat::chat_clientClient& client, const std::string& user_name)
-			: client(client), user_name(user_name)
-		{
-		}
-
-		example::chat::chat_clientClient client;
-		std::string user_name;
-	};
-
-	typedef std::shared_ptr<Session> session_ptr;
-
-	typedef std::map<protocol_ptr, session_ptr> Sessions;
-	Sessions sessions_;
-
-	session_ptr current_session_;
 };
 
 
@@ -122,7 +71,7 @@ void the_blocking_version(boost::asio::io_service& io_service)
  * */
 void the_non_blocking_loop(boost::asio::io_service& io_service)
 {
-	while(true)
+	while (true)
 	{
 		while (io_service.poll_one());
 
